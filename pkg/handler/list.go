c85e9c55dabc586d8270bf5c54f373f763623aea
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-05 10:15:58 +0600

    create method is finished partly

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,52 +1,30 @@
 package handler
 
 import (
-	"context"
-	"log"
+	"net/http"
 
+	"github.com/ctuzelov/region-todo/pkg/models"
+	"github.com/ctuzelov/region-todo/util"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (h *Handler) createList(g *gin.Context) {
-	// Get a handle to the "users" collection.
-	collection := h.service.ToDoList.Database("user").Collection("users")
-
-	user := User{
-		Name: "John Doe",
+	var input models.Task
+	if err := g.BindJSON(&input); err != nil {
+		NewErrorResponse(g, http.StatusBadRequest, err.Error())
+		return
 	}
-
-	// Insert the document into the "users" collection.
-	_, err = collection.InsertOne(context.Background(), user)
+	id, err := h.service.ToDoList.CreateTask(input)
 	if err != nil {
-		log.Fatal("Error inserting document:", err)
+		NewErrorResponse(g, http.StatusUnauthorized, err.Error())
+		return
 	}
 
-	// Define a filter to match all documents (an empty filter means all documents).
-	filter := bson.M{}
-
-	// Execute the query and get the cursor to the results.
-	cursor, err := collection.Find(context.Background(), filter)
-	if err != nil {
-		log.Fatal("Error executing the query:", err)
-	}
-	defer cursor.Close(context.Background())
-
-	// Iterate over the cursor to access the documents.
-	var users []User
-	for cursor.Next(context.Background()) {
-		var user User
-		if err := cursor.Decode(&user); err != nil {
-			log.Fatal("Error decoding document:", err)
-		}
-		users = append(users, user)
-	}
-
-	// Handle any errors that occurred during iteration.
-	if err := cursor.Err(); err != nil {
-		log.Fatal("Cursor error:", err)
-	}
-
+	g.JSON(http.StatusOK, models.Task{
+		ID:       util.IntToByteArray(id),
+		Title:    input.Title,
+		ActiveAt: input.ActiveAt,
+	})
 }
 
 func (h *Handler) getListByID(g *gin.Context) {
