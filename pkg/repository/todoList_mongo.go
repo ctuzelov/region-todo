b792a4f5f6ffb33a06c7e1a4bb46e82e28b2fec2
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-05 14:02:25 +0600

    trash

--- pkg/repository/todoList_mongo.go
+++ pkg/repository/todoList_mongo.go
@@ -5,9 +5,7 @@ import (
 	"log"
 
 	"github.com/ctuzelov/region-todo/pkg/models"
-	"github.com/ctuzelov/region-todo/util"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,40 +18,30 @@ func NewToDoListMongoDB(db *mongo.Client) *ToDoListMongoDB {
 }
 
 func (r *ToDoListMongoDB) CreateTask(task models.Task) (int, error) {
-	// Get a handle to the "users" collection.
-	collection := r.db.Database("tasks").Collection("todo_list")
+	// Получение ссылки на коллекцию "tasks" в базе данных "testdb"
+	collection := r.db.Database("testdb").Collection("tasks")
 
-	// Insert the document into the "users" collection.
-	inserted, err := collection.InsertOne(context.Background(), task)
-	insertedID := inserted.InsertedID.(primitive.ObjectID)
+	// Создание счетчика, если его нет
+	countersCollection := r.db.Database("testdb").Collection("counters")
+	countersCollection.InsertOne(context.Background(), models.Counter{ID: "taskID", Sequence: 0})
+
+	// Генерация нового _id на основе счетчика
+	var newID int
+	err := countersCollection.FindOneAndUpdate(context.Background(), bson.M{"_id": "taskID"}, bson.M{"$inc": bson.M{"seq": 1}}).Decode(&models.Counter{})
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
 
-	// Define a filter to match all documents (an empty filter means all documents).
-	filter := bson.M{}
-
-	// Execute the query and get the cursor to the results.
-	cursor, err := collection.Find(context.Background(), filter)
+	// Получение текущего значения счетчика
+	err = countersCollection.FindOne(context.Background(), bson.M{"_id": "taskID"}).Decode(&models.Counter{})
 	if err != nil {
-		return 0, err
-	}
-	defer cursor.Close(context.Background())
-
-	// Iterate over the cursor to access the documents.
-	var tasks []models.Task
-	for cursor.Next(context.Background()) {
-		var todo models.Task
-		if err := cursor.Decode(&todo); err != nil {
-			return 0, err
-		}
-		tasks = append(tasks, todo)
+		log.Fatal(err)
 	}
 
-	// Handle any errors that occurred during iteration.
-	if err := cursor.Err(); err != nil {
-		log.Fatal("Cursor error:", err)
+	// Вставка документа в MongoDB
+	_, err = collection.InsertOne(context.Background(), task)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	return util.ByteArrayToInt(insertedID), nil
+	return newID, nil
 }
