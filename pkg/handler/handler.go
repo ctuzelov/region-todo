ddba036e4ab8f676987f68d82dc27672cd19a181
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-05 10:14:09 +0600

    router is finished

---
+++ pkg/handler/handler.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"github.com/ctuzelov/region-todo/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{service: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	tasks := router.Group("/api/todo-list/tasks")
+
+	tasks.POST("/", h.createList)
+	tasks.GET("/:id", h.getListByID)
+	tasks.PUT("/:id", h.updateListByID)
+	tasks.DELETE("/:id", h.deleteListByID)
+
+	return router
+}
