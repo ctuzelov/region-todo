ff3f3ff99ce76fbdd49e626e399c1a7931d7758b
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-05 10:19:42 +0600

    deleted unnecessary auth interface

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,21 +1,20 @@
 package service
 
-import "github.com/ctuzelov/region-todo/pkg/repository"
-
-type Authorization interface {
-}
+import (
+	"github.com/ctuzelov/region-todo/pkg/models"
+	"github.com/ctuzelov/region-todo/pkg/repository"
+)
 
 type ToDoList interface {
+	CreateTask(task models.Task) (int, error)
 }
 
 type Service struct {
-	Authorization
 	ToDoList
 }
 
 func NewService(repo *repository.Repository) *Service {
 	return &Service{
-		Authorization: NewAuthService(repo.Authorization),
-		ToDoList:      NewToDoService(repo.ToDoList),
+		NewToDoService(repo.ToDoList),
 	}
 }
