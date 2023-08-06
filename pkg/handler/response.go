be5d0cb99265939cae09928d21daa4e3198d4044
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-06 16:18:29 +0600

    new error

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
@@ -8,6 +10,9 @@ import (
 type errorResponse struct {
 	Message string `json:"message"`
 }
+
+var errForm = errors.New("follow next rules: title length <= 200, ativeAt format 2023-01-01")
+var errDuplicate = errors.New("a task with such values already exists")
 
 func NewErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Error(message)
