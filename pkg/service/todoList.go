9fd44a8c001390c2fbf4206d2df6565d8c0dedb4
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-06 16:19:00 +0600

    described methods

--- pkg/service/todoList.go
+++ pkg/service/todoList.go
@@ -16,3 +16,19 @@ func NewToDoService(repo repository.ToDoList) *ToDoListService {
 func (s *ToDoListService) CreateTask(task models.Task) (int, error) {
 	return s.repo.CreateTask(task)
 }
+
+func (s *ToDoListService) ReadTask(id int) (models.Task, error) {
+	return s.repo.ReadTask(id)
+}
+
+func (s *ToDoListService) Delete(id int) error {
+	return s.repo.Delete(id)
+}
+
+func (s *ToDoListService) UpdateStatus(id int) error {
+	return s.repo.UpdateStatus(id)
+}
+
+func (s *ToDoListService) UpdateTask(id int, task models.Task) error {
+	return s.repo.UpdateTask(id, task)
+}
