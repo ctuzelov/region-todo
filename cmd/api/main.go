4c422836089f784bbff8975038e64ef43f5d3478
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-08 12:04:10 +0600

    adding shotdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,16 +4,37 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ctuzelov/region-todo/cmd/server"
+	_ "github.com/ctuzelov/region-todo/docs"
 	"github.com/ctuzelov/region-todo/pkg/handler"
 	"github.com/ctuzelov/region-todo/pkg/repository"
 	"github.com/ctuzelov/region-todo/pkg/service"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
+// @title TODO TASKS API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+
 func main() {
-	if err := initConfig(); err != nil {
+	if err := server.InitConfig(); err != nil {
 		log.Fatalf("error %s occured while initializating configs", err)
 	}
 
@@ -37,14 +58,20 @@ func main() {
 	service := service.NewService(repo)
 	handler := handler.NewHandler(service)
 	srv := new(server.Server)
-	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
-		log.Fatalf("error %s in runnig the http server", err)
-	}
-	log.Printf("listening on http://localhost" + viper.GetString("port"))
-}
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+			log.Fatalf("error %s in runnig the http server", err)
+		}
+	}()
+	log.Printf("listening on http://localhost:" + viper.GetString("port"))
 
-func initConfig() error {
-	viper.AddConfigPath("pkg/configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
 }
